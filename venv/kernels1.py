0d77ef1fdf82eefb1d369d73ecb5f57ec49173e6
wa008 <2864436211@qq.com>
2019-03-25 23:17:28 +0800

    begin study kernels1_data_analysis

--- venv/kernels1.py
+++ venv/kernels1.py
@@ -11,4 +11,11 @@ import warnings
 warnings.filterwarnings('ignore')
 
 df_train = pd.read_csv("input_data/train.csv")
-print(df_train.columns)
+# print(df_train.columns)
+# sns.distplot(df_train['SalePrice']) # 将SalePrice可视化
+
+var = 'TotalBsmtSF'
+data = pd.concat([df_train['SalePrice'], df_train[var]], axis = 1)
+data.plot.scatter(x = var, y = 'SalePrice')
+plt.savefig('pictures/TotalBsmtSF_SalePrice.png') # 线性相关
+plt.show()
