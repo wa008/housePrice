c4a93353588137beb379d032366a7f91b3ea67fa
wa008 <2864436211@qq.com>
2019-04-09 11:38:39 +0800

    update pictures

--- venv/kernels1.py
+++ venv/kernels1.py
@@ -1,8 +1,8 @@
 # kaggle_kernels_first
 #invite people for the Kaggle party
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import norm
 from sklearn.preprocessing import StandardScaler
@@ -31,8 +31,12 @@ df_train = pd.read_csv("input_data/train.csv")
 # plt.show()
 
 # corrmat = df_train.corr()
-# f,ax = plt.subplots(figsize = (12,9))
-# sns.heatmap(corrmat, vmax = 1,square = True) # 相关矩阵和热力图
+# # f, ax = plt.subplots(figsize = (12,10))
+# sns.heatmap(corrmat, vmax = .8,square = True) # 相关矩阵和热力图
+# plt.title("data heatmap picture")
+# plt.xticks(rotation=90)
+# plt.yticks(rotation=360)
+# plt.show()
 
 # k = 10
 # cols = corrmat.nlargest(k, 'SalePrice')['SalePrice'].index
@@ -40,6 +44,10 @@ df_train = pd.read_csv("input_data/train.csv")
 # sns.set(font_scale=1.25)
 # hm = sns.heatmap(cm, cbar=True, annot=True, square=True,fmt='.2f',annot_kws={'size':10},
 #                  yticklabels=cols.values, xticklabels=cols.values)
+# plt.title("zommed data heatmap picture")
+# plt.xticks(rotation=90)
+# plt.yticks(rotation=360)
+# plt.show()
 
 # sns.set()
 # cols = ['SalePrice', 'OverallQual', 'GrLivArea', 'GarageCars',
@@ -49,11 +57,11 @@ df_train = pd.read_csv("input_data/train.csv")
 # plt.show()
 
 # 4.缺失值处理
-total = df_train.isnull().sum().sort_values(ascending=False)
-percent = (df_train.isnull().sum()/1460)\
-    .sort_values(ascending=False)
-missing_data = pd.concat([total,percent],axis=1,keys=['Total','Percent'])
-print(missing_data.head(20))
+# total = df_train.isnull().sum().sort_values(ascending=False)
+# percent = (df_train.isnull().sum()/1460)\
+#     .sort_values(ascending=False)
+# missing_data = pd.concat([total,percent],axis=1,keys=['Total','Percent'])
+# print(missing_data.head(20))
 #
 # df_train = df_train.drop((missing_data[missing_data['Total']>1]).index,1)
 # df_train = df_train.drop(df_train.loc[df_train['Electrical'].isnull()].index)
@@ -76,13 +84,21 @@ df_train = df_train.drop(df_train[df_train['Id']==524].index)
 # data = pd.concat([df_train['SalePrice'], df_train[var]], axis = 1)
 # data.plot.scatter(x=var, y='SalePrice',ylim=(0,800000))
 
+
 df_train['SalePrice'] = np.log(df_train['SalePrice'])
 df_train['GrLivArea'] = np.log(df_train['GrLivArea'])
+
+sns.distplot(df_train['SalePrice'],fit=norm)
+fig = plt.figure()
+res = stats.probplot(df_train['SalePrice'], plot=plt)
+plt.show()
+
 df_train['HasBsmt'] = pd.Series(len(df_train['TotalBsmtSF']),index
             =df_train.index)
 df_train['HasBsmt'] = 0
 df_train.loc[df_train['TotalBsmtSF']>0, 'HasBsmt'] = 1
 df_train.loc[df_train['HasBsmt']==1,'TotalBsmtSF'] = np.log(df_train['TotalBsmtSF'])
+
 
 # sns.distplot(df_train[df_train['TotalBsmtSF']>0]['TotalBsmtSF'],fit = norm)
 # fig = plt.figure()
